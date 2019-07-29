71e7768346bd299d5d983139a320aebc6174f261
JP Wesselink <jpwesselink@gmail.com>
2019-07-29 08:37:03 +0200

    Applied styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { ThemeProvider } from 'styled-components';
 
 import withApolloClient from '../lib/hoc/with-apollo-client';
+import theme from '../lib/styled-components-theme';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
+        <ThemeProvider theme={theme}>
+          <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </ThemeProvider>
       </Container>
     );
   }
